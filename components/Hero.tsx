d12b0a61bb1f429ec90bb00af1e2dc1297152909
Deepak Singh <deepaksiinggh@gmail.com>
2026-05-24 13:55:17 +0530

    Updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
